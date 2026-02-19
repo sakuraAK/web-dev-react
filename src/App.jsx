37c306853375703f8bc95aa7107bac0774ae0901
sakuraAK <andrey.krutauz@gmail.com>
2026-02-19 17:56:15 -0500

    week 2: add dynamic list renedering

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,50 @@ import TaskCard from './components/TaskCard/TaskCard'
 
 const name = "Andrey";
 
-
+const tasks = [
+  {
+    title: "Task 1",
+    dueDate: "Tomrrow",
+    priority: "High",
+    assignedTo: "Andrey",
+    status: "Pending",
+  },
+   {
+    title: "Task 2",
+    dueDate: "Tomrrow",
+    priority: "High",
+    assignedTo: "Andrey",
+    status: "Pending",
+  },
+   {
+    title: "Task 3",
+    dueDate: "Tomrrow",
+    priority: "High",
+    assignedTo: "Andrey",
+    status: "Pending",
+  },
+];
 
 function App() {
+
   return (
   <>
     <h1>Hello {name} below are your tasks:</h1>
-    <TaskCard 
-      title="Task 1" 
-      dueDate="Tomorrow" 
-      priority="High" 
-      assignedTo="Andrey"
-    />
+    {
+      tasks.map((task) => (
+        <TaskCard
+          {...task}
+          key={task.title}
+          // title={task.title}
+          // dueDate={task.dueDate}
+          // priority={task.priority}
+          // assignedTo={task.assignedTo}
+        />
+      )
+
+      )
+    }
+    
   </>
 );
 
