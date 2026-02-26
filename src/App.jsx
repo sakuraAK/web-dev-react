eb4bbf0037db849cdf2c417d8d03a5aef416889a
sakuraAK <andrey.krutauz@gmail.com>
2026-02-26 17:41:25 -0500

    week 3 work in class

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,107 @@
+import { useState } from 'react';
 import './App.css'
-import TaskCard from './components/TaskCard/TaskCard'
+import './components/TaskCard/TaskCard.css'
+
 
 const name = "Andrey";
 
-const tasks = [
-  {
-    title: "Task 1",
-    dueDate: "Tomrrow",
-    priority: "High",
-    assignedTo: "Andrey",
-    status: "Pending",
-  },
-   {
-    title: "Task 2",
-    dueDate: "Tomrrow",
-    priority: "High",
-    assignedTo: "Andrey",
-    status: "Pending",
-  },
-   {
-    title: "Task 3",
-    dueDate: "Tomrrow",
-    priority: "High",
-    assignedTo: "Andrey",
-    status: "Pending",
-  },
-];
 
 function App() {
+
+  const [tasks, updateTasks] =  useState([]);
+  const [formData, updateFormData] = useState({
+    title: "",
+    priority: "High",
+    dueDate: "",
+  });
+
+ 
+
+  function addTask() {
+
+  
+    if (formData.title.trim() === "") {
+      return;
+    }
+
+
+    const newTask = {
+      id: Date.now(),
+      title: formData.title,
+      priority: formData.priority,
+      dueDate: formData.dueDate,
+    }
+
+
+
+    updateTasks([...tasks, newTask]);
+
+    updateFormData({
+      title: "",
+      priority: "High",
+      dueDate: "",
+    });
+  }
 
   return (
   <>
     <h1>Hello {name} below are your tasks:</h1>
-    {
-      tasks.map((task) => (
-        <TaskCard
-          {...task}
-          key={task.title}
-          // title={task.title}
-          // dueDate={task.dueDate}
-          // priority={task.priority}
-          // assignedTo={task.assignedTo}
+    <form onSubmit={(e) => {
+          e.preventDefault();
+          addTask();
+       }}>
+      <div>
+        <label>Task name:</label>
+        <input
+          type='text'
+          name='title'
+          value={formData.title}
+          onChange={(e) => updateFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+          })}
         />
-      )
-
-      )
+          { formData.title.trim() === "" && <p className="validation">Title required</p>}
+      </div>
+      <div>
+        <label>Priority:</label>
+        <select
+          name='priority'
+          value={formData.priority}
+          onChange={(e) => updateFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+          })}
+        >
+          <option value="High">High</option>
+          <option value="Medium">Medium</option>
+          <option value="Low">Low</option>
+        </select>
+       
+      </div>
+      <div>
+        <label>Due date:</label>
+        <input
+          type='text'
+          name='dueDate'
+          value={formData.dueDate}
+          onChange={(e) => updateFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+          })}
+        />
+      </div>
+      <button>Add</button>
+    </form>
+    
+    {
+      tasks.map(task => (
+        <div key={task.id}>
+          <h3>{task.title}</h3>
+          <p>{task.priority}</p>
+          <p>{task.dueDate}</p>
+        </div>
+      ))
     }
     
   </>
