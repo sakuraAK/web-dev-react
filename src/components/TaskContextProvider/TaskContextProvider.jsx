5ac880d7b87d9617b05241c85874659e1f47eb59
Andrey <andrey.krutauz@gmail.com>
2026-04-18 13:22:54 -0400

    Add backend code. Implement get all tasks and create new one.

--- src/components/TaskContextProvider/TaskContextProvider.jsx
+++ src/components/TaskContextProvider/TaskContextProvider.jsx
@@ -2,53 +2,83 @@ import TaskContext from "../../context/TaskContext";
 import { useState, useEffect } from "react";
 
 function TaskContextProvider({ children }) {
+  //  const [tasks, updateTasks] = useState(() => {
+  //     const savedTasks = localStorage.getItem("tasks");
+  //     return savedTasks ? JSON.parse(savedTasks) : [];
+  //   });
 
-     const [tasks, updateTasks] = useState(() => {
-        const savedTasks = localStorage.getItem("tasks");
-        return savedTasks ? JSON.parse(savedTasks) : [];
+  const [tasks, updateTasks] = useState([]);
+  const [loading, updateLoading] = useState(true);
+  const [error, updateError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:3000/tasks");
+        if (!response.ok) {
+          throw new Error("Failed to fetch tasks");
+        }
+        console.log(response);
+
+        const data = await response.json();
+
+        updateTasks(data.tasks);
+      } catch (err) {
+        updateError("Failed to load tasks" + err);
+      } finally {
+        updateLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  // useEffect(() => {
+  //   localStorage.setItem("tasks", JSON.stringify(tasks));
+  // }, [tasks]);
+
+  function addTask(formData) {
+    if (formData.title.trim() === "") {
+      return;
+    }
+
+    const newTask = {
+      title: formData.title,
+      priority: formData.priority,
+      dueDate: formData.dueDate,
+    };
+
+    fetch("http://localhost:3000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTask),
+    })
+      .then((response) => {
+        if (!response.ok) throw Error("Unable to add a new task");
+        return response.json();
+      })
+      .then((data) => updateTasks([...tasks, data.task]))
+      .catch((err) => {
+        updateError("Failed to add task error: " + err.message);
       });
-    
-      useEffect(() => {
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-      }, [tasks]);
-    
-      function addTask(formData) {
-    
-        if (formData.title.trim() === "") {
-          return;
-        }
-    
-        const newTask = {
-          id: Date.now(),
-          title: formData.title,
-          priority: formData.priority,
-          dueDate: formData.dueDate,
-        }
-    
-        updateTasks([...tasks, newTask]);
-    
-      }
-    
-      function onDeleteTask(id) {
-        console.log("Deleting task");
-        updateTasks([...tasks.filter(t => t.id !== id)]);
-      }
+  }
 
-      const value = {
-        tasks,
-        addTask,
-        onDeleteTask,
-      }
+  function onDeleteTask(id) {
+    console.log("Deleting task");
+    updateTasks([...tasks.filter((t) => t.id !== id)]);
+  }
 
-      return (
-        <TaskContext.Provider value={value}>
-            {children}
-        </TaskContext.Provider>
-      );
+  const value = {
+    tasks,
+    loading,
+    error,
+    addTask,
+    onDeleteTask,
+  };
 
-
-
+  return <TaskContext.Provider value={value}>{children}</TaskContext.Provider>;
 }
-
 
 export default TaskContextProvider;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,31 @@
 import TaskList from "../components/TaskList/TaskList";
 import TaskForm from "../components/TaskForm/TaskForm";
+import { useContext } from "react";
+import TaskContext from "../context/TaskContext";
 
 
 function HomePage() {
+    const {loading, error} = useContext(TaskContext);
+
+    let content = (
+      <>
+        <TaskForm />
+        <TaskList />
+      </>
+    );
+
+    if (loading) {
+        content = (<p>Loading...</p>);
+    }
+    if (error) {
+        content = (<p>{error}</p>);
+    }
+
     return (
         <>
             <h1>Hello below are your tasks:</h1>
-            <TaskForm/>  
-            <TaskList/>
+
+           { content }
         </>
     );
 }
